6b804b35e96a1486459829c03e31b147779999fc
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-06-29 01:23:52 +0200

    Create index.vue

---
+++ computed/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { JSONValue } from '../utils/json'
+import JsonViewer from './JsonViewer'
+
+defineProps<{
+  data: JSONValue
+}>()
+</script>
+
+<template>
+  <div v-if="data" class="rounded font-mono text-sm">
+    <JsonViewer :data="data" />
+  </div>
+  <p v-else class="text-neutral-500/50">
+    No data
+  </p>
+</template>
